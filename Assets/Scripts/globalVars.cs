4fcf718f19148fcab8c8b3035bd5d46701813840
James Foster <englandjf@yahoo.com>
2016-02-18 11:26:44 -0800

    Comments and clean up for global vars

--- Assets/Scripts/globalVars.cs
+++ Assets/Scripts/globalVars.cs
@@ -9,29 +9,42 @@ public class globalVars : MonoBehaviour {
 
 	public string filePath;
 
+	//assigned when the mouse/touch is over an object
 	public GameObject overObject;
+	//assigned when the mouse/touch is over an object and is clicked
 	public GameObject selectedObject;
 
 	//Control which camera is active
+	//Camera for the main track 
 	public Camera mainCam;
+	//Camera for the group/mixer settings
 	public Camera mixerCam;
+	//Camera for the sound editor
 	public Camera soundCam;
+	//Camera for the Free Sound search/download
 	public Camera importCam;
+	//Camera that is active
 	public Camera current;
+
 	//Control which canvas is active
+	//Canvas for the main track
 	public Canvas mainCanvas; 
+	//Canvas for group/mixer settings
 	public Canvas mixerCanvas;
+	//Canvas for the sound editor
 	public Canvas soundCanvas;
+	//Canvas for the Free Sound search/download
 	public Canvas importCanvas;
+	//Canvas that is active
 	public Canvas currentCanvas;
 
-	//To handle groups
+	//To handle all groups
 	public Hashtable allGroups;
 
-	//Increment soundName 
+	//Increment soundName
 	public int soundNumber = 1;
 
-	//Play click(metronome)
+	//Play click(metronome) if enabled
 	public Toggle clickToggle;
 
 	//All created sounds(master)
@@ -43,9 +56,13 @@ public class globalVars : MonoBehaviour {
 
 	//For snap
 	public Slider snapSelector;
+	//Snap is on when enabled
 	public Toggle snapping;
+	//Snaps to metronome if enabled
 	public Toggle metroSnap;
+	//Shows snaps per second
 	public Text snapText;
+	//Starting snap
 	public float currentSnap = 1.0f;
 
 	//For editing sounds
@@ -61,6 +78,7 @@ public class globalVars : MonoBehaviour {
 		//set file path
 		filePath =  Application.dataPath+ "/Downloads/";
 
+		//Setup tables for groups
 		allGroups = new Hashtable ();
 		allSounds = new List<AudioSource> ();
 		allGroupA = new List<AudioSource> ();
@@ -68,30 +86,25 @@ public class globalVars : MonoBehaviour {
 		allGroups.Add ("GroupA", allGroupA);
 
 		//File.Delete(Application.persistentDataPath+ "/dataFile");
+
+		//verifies if the API token is valid
 		checkTokenInfo ();
-		//validReady = checkTokenInfo ();
-		//if(!validReady)
-		//	GameObject.Find ("importCam").GetComponent<importScript> ().refreshToken ();
-
-		//Debug.Log (System.DateTime.Now.);
-
-		//hideButton (deleteSound);
-
-		//Debug.Log (HttpGet ( "https://www.freesound.org/apiv2/sounds/333489/download/"));
-		//StartCoroutine(HttpGet());
 	}
 
 	//for free sound 
+	//True if there is no file information for Free Sound
 	public bool firstTimeLoad;
+	//True if token is valid
 	public bool validReady;
+	//Stores the access token used for Free Sound
 	public string accessToken;
 
+	//Verifies the validity of the Free Sound token in the file system
 	void checkTokenInfo()
 	{
-		//first check if this file is even there
+		//Checks if the file is present
 		if (File.Exists (Application.persistentDataPath + "/dataFile")) {
-			//StreamReader a = File.OpenRead (Application.dataPath + "/dataFile");
-			importScript.accessInfo temp = importScript.accessInfo.createFromJSON (File.ReadAllText(Application.persistentDataPath + "/dataFile"));
+			importScript.accessInfo temp = importScript.accessInfo.createFromJSON (System.IO.File.ReadAllText(Application.persistentDataPath + "/dataFile"));
 			System.DateTime actualTime = System.DateTime.Parse(temp.expireTime);
 			//1 if date is valid, -1 otherwise
 			if (System.DateTime.Compare (actualTime, System.DateTime.Now) != -1) {
@@ -105,80 +118,53 @@ public class globalVars : MonoBehaviour {
 				} else {
 					//error
 				}
-				//return false;
-				//GameObject.Find ("importCam").GetComponent<importScript> ().refreshToken ();
 			}
 			Debug.Log (System.DateTime.Compare (actualTime, System.DateTime.Now));
-		} else {
+		}
+		//If not present
+		else {
 			//fresh login
 			firstTimeLoad = true;
 			validReady = false;
 		}
 	}
 
-	string URL = "https://www.freesound.org/apiv2/sounds/333489/download/";
-	//test
-	//IEnumerator HttpGet()
-	//{
 
-
-	//}
 
 	//For double clicking
+	/*
 	bool oneClick = false;
 	bool timerRunning;
 	float timerDC;
 	float delay = .25f;
+	*/
 	
 	// Update is called once per frame
 	void Update () {
 
-
+		//Will need to change this, but is used to get to group/mixer options
 		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
 			cameraHelp (mixerCam, mixerCanvas);
-			/*
-			mainCam.enabled = false;
-			mainCanvas.enabled = false;
-			mixerCam.enabled = true;
-			mixerCanvas.enabled = true;
-			soundCam.enabled = false;
-			soundCanvas.enabled = false;
-			current = mixerCam;
-			currentCanvas = mixerCanvas;
-			*/
 		}
-		//Main
+		//Will need to change this, but goes to main track
 		else if (Input.GetKeyDown (KeyCode.RightArrow)) {
 			cameraHelp (mainCam, mainCanvas);
-			/*
-			mainCam.enabled = true;
-			mainCanvas.enabled = true;
-			mixerCam.enabled = false;
-			mixerCanvas.enabled = false;
-			soundCam.enabled = false;
-			soundCanvas.enabled = false;
-			current = mainCam;
-			currentCanvas = mainCanvas;
-			*/
 		}
-
-		//scroll, mostly just in editor
+			
+		//scroll, mostly just in editor, will probably eventually take out 
 		/*
 		if(current == mainCam && Input.GetMouseButton(1)){
 			if (Input.mousePosition.x > Screen.width/2) {
 				mainCam.transform.position = new Vector3 (mainCam.transform.position.x + .05f, mainCam.transform.position.y, mainCam.transform.position.z);
 			} else if (Input.mousePosition.x < Screen.width/2) {
 				mainCam.transform.position = new Vector3 (mainCam.transform.position.x - .05f, mainCam.transform.position.y, mainCam.transform.position.z);
-			}
-
-				
-
+			}	
 		}
 		*/
 
 
-
-		//not working correctly, might just make button
+		//double click
+		//not working correctly, might just make button, will take out soon
 		/*
 		if (Input.GetMouseButtonDown (0) && selectedObject) {
 			if (!oneClick) {
@@ -195,62 +181,18 @@ public class globalVars : MonoBehaviour {
 				oneClick = false;
 		}
 		*/
-
-		//Hide/Show Edit Button
-		//if (selectedObject)
-		//	editSound.enabled = true;
-		//else
-		//	editSound.enabled = false;
-
-		//Camera control
-		//Different controls if inside 
-		/*
-		if (!soundCam.enabled) {
-			//Mixer
-			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
-				mainCam.enabled = false;
-				mixerCam.enabled = true;
-				soundCam.enabled = false;
-				current = mixerCam;
-			}
-			//Main
-			else if (Input.GetKeyDown (KeyCode.RightArrow)) {
-				mainCam.enabled = true;
-				mixerCam.enabled = false;
-				soundCam.enabled = false;
-				current = mainCam;
-			}
-			//Sound
-			else if (Input.GetKeyDown (KeyCode.UpArrow) && current == mainCam) {
-				mainCam.enabled = false;
-				mixerCam.enabled = false;
-				soundCam.enabled = true;
-				current = soundCam;
-			}
-		} 
-		else {
-			//To get back to main
-			if(Input.GetKeyDown(KeyCode.DownArrow))
-			{
-				mainCam.enabled = true;
-				mixerCam.enabled = false;
-				soundCam.enabled = false;
-				current = mainCam;
-			}
-		}
-		*/
-
-
 	}
 
 
-
+	//Drop down to show group options, might not include in first version because of stability
 	public Dropdown groupOptions;
+	//Not currently used
+	/*
 	public void updateSelectedGroup()
 	{
 		selectedObject.GetComponent<soundScript> ().groupName = groupOptions.options[groupOptions.value].text;
 		selectedObject.GetComponent<soundScript> ().updateSoundSettings (false);
-		/*
+
 		string tempName = selectedObject.GetComponent<soundScript> ().groupName;
 		//remove from other list if needed and clear features
 		if (tempName != "") {
@@ -258,8 +200,9 @@ public class globalVars : MonoBehaviour {
 			a.Remove(selectedObject.GetComponent<AudioSource>());
 			//disable features
 		}
-		*/
+
 	}
+	*/
 
 	//Dropdown for sounds
 	public Dropdown soundOptions;
@@ -272,72 +215,35 @@ public class globalVars : MonoBehaviour {
 	}
 
 	//Launch edit menu for selected sound
-	//Go to edit menu
 	public void launchEditMenu()
 	{
 		if (selectedObject) {
 			cameraHelp (soundCam, soundCanvas);
-			/*
-			mainCam.enabled = false;
-			mainCanvas.enabled = false;
-			mixerCam.enabled = false;
-			mixerCanvas.enabled = false;
-			soundCam.enabled = true;
-			soundCanvas.enabled = true;
-			current = soundCam;
-			currentCanvas = soundCanvas;
-			*/
-			//Set selected object to be edited
 			soundCam.GetComponent<soundEditScript> ().soundReference = selectedObject;
 			soundCam.GetComponent<soundEditScript> ().enabled = true;
 		}
 	}
-	//with group options
+
+	//Launch edit menu with group 
 	public void launchEditMenu(string group)
 	{
 		cameraHelp (soundCam, soundCanvas);
-		/*
-		mainCam.enabled = false;
-		mainCanvas.enabled = false;
-		mixerCam.enabled = false;
-		mixerCanvas.enabled = false;
-		soundCam.enabled = true;
-		soundCanvas.enabled = true;
-		current = soundCam;
-		currentCanvas = soundCanvas;
-		*/
 		//Set selected object to be edited
 		soundEditScript temp = soundCam.GetComponent<soundEditScript> ();
 		temp.groupName = group;
 		temp.enabled = true;
 	}
 		
-
+	//Return to main track
 	public void backToMain()
 	{
 		
 		soundEditScript temp = current.gameObject.GetComponent<soundEditScript> ();
-		//temp.soundReference = null;
-		//temp.groupName = "";
 		temp.enabled = false;
-
 		cameraHelp (mainCam, mainCanvas);
-		/*
-		mainCam.enabled = true;
-		mainCanvas.enabled = true;
-		mixerCam.enabled = false;
-		mixerCanvas.enabled = false;
-		soundCam.enabled = false;
-		soundCanvas.enabled = false;
-		current = mainCam;
-		currentCanvas = mainCanvas;
-		*/
-		//Disable script
-
-
-
 	}
-		
+
+	//Called when the snap value is modified 
 	public void snapChanged()
 	{
 		currentSnap = 1 / snapSelector.value;
@@ -348,18 +254,10 @@ public class globalVars : MonoBehaviour {
 	//Go to import menu
 	public void launchImport()
 	{
-		/*
-		current.enabled = false;
-		currentCanvas.enabled = false;
-		current = importCam;
-		currentCanvas = importCanvas;
-		current.enabled = true;
-		current.enabled = true;
-		*/
 		cameraHelp (importCam,importCanvas);
 	}
 
-	//helper for changing cams
+	//Helper for changing cams
 	void cameraHelp(Camera a,Canvas b)
 	{
 		Debug.Log (current.name);
@@ -381,19 +279,25 @@ public class globalVars : MonoBehaviour {
 			selectedObject.GetComponent<soundScript> ().deleteSound ();
 	}
 
+	//might use later
+	/*
 	void hideButton(Button a)
 	{
 		deleteSound.enabled = false;
 		deleteSound.GetComponent<Image> ().material.color = Color.clear;
 		deleteSound.GetComponentInChildren<Text> ().color = Color.clear;
-	}
+	*/
 
+	/*
 	void showButton(Button a)
 	{
 		
 	}
+	*/
 
+	//For modifying song settings
 	public GameObject songInfo;
+	//Called to show/hide song settings
 	public void showSongInfo(bool show)
 	{
 		if(show)
